427f1c7b3f8486f93ab5c5303a44cac5f18ee889
mompei <ogunborjerry@gmail.com>
2024-12-14 03:36:54 +0100

    connection established

---
+++ multithreaded_web_server/hello/target/debug/deps/hello-bd034b05d2221d72.d
@@ -0,0 +1,5 @@
+/home/mompei/rustc/multithreaded_web_server/hello/target/debug/deps/hello-bd034b05d2221d72: src/main.rs
+
+/home/mompei/rustc/multithreaded_web_server/hello/target/debug/deps/hello-bd034b05d2221d72.d: src/main.rs
+
+src/main.rs:
---
+++ multithreaded_web_server/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/mompei/rustc/multithreaded_web_server/hello/target/debug/hello: /home/mompei/rustc/multithreaded_web_server/hello/src/main.rs
